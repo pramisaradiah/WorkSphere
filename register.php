e1753edb978827c2ca030520647d8f95ccf37a67
pramisaradiah <diahpramisara@gmail.com>
2025-12-17 13:24:03 +0800

    Update file register

--- register.php
+++ register.php
@@ -1,6 +1,7 @@
 <?php
 require "fungsi.php";
 
+$error = "";
 
 if (isset($_POST['signup'])) {
     $email = trim($_POST['email']);
@@ -9,17 +10,26 @@ if (isset($_POST['signup'])) {
     if ($email === "" || $password === "") {
         $error = "Email dan password wajib diisi";
     } else {
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-        $query = "INSERT INTO users (email, password) VALUES (?, ?)";
-        $stmt = mysqli_prepare($conn, $query);
-        mysqli_stmt_bind_param($stmt, "ss", $email, $hashedPassword);
+        // CEK EMAIL SUDAH ADA ATAU BELUM
+        $check = mysqli_prepare($conn, "SELECT id_user FROM users WHERE email = ?");
+        mysqli_stmt_bind_param($check, "s", $email);
+        mysqli_stmt_execute($check);
+        mysqli_stmt_store_result($check);
 
-        if (mysqli_stmt_execute($stmt)) {
+        if (mysqli_stmt_num_rows($check) > 0) {
+            $error = "Email sudah terdaftar";
+        } else {
+
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+            $query = "INSERT INTO users (email, password) VALUES (?, ?)";
+            $stmt = mysqli_prepare($conn, $query);
+            mysqli_stmt_bind_param($stmt, "ss", $email, $hashedPassword);
+            mysqli_stmt_execute($stmt);
+
             header("Location: role.php");
             exit;
-        } else {
-            $error = "Email sudah terdaftar";
         }
     }
 }
@@ -35,19 +45,27 @@ if (isset($_POST['signup'])) {
 
 <h2>Create Account</h2>
 
-<?php if ($error): ?>
-    <p><?= $error ?></p>
-<?php endif; ?>
+<p>
+    Already have an account?
+    <a href="role.php">Sign In</a>
+</p>
 
 <form method="POST">
     <label>Email</label><br>
-    <input type="email" name="email" required><br><br>
+    <input type="email" name="email"
+           value="<?= isset($email) ? htmlspecialchars($email) : '' ?>"
+           required><br><br>
 
     <label>Password</label><br>
     <input type="password" name="password" required><br><br>
 
     <button type="submit" name="signup">Sign Up</button>
 </form>
+
+<!-- ERROR DI BAWAH FORM -->
+<?php if ($error): ?>
+    <p><?= $error ?></p>
+<?php endif; ?>
 
 </body>
 </html>
